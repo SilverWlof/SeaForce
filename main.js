5265c224a17f789aad408eb50b456b413fa1dfbf
Mikhail <33647581+SilverWlof@users.noreply.github.com>
2025-09-19 02:06:10 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -7,6 +7,7 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
 /***/ "./src/components/index.js":
@@ -15,18 +16,7 @@
   \*********************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-"use strict";
-eval("{__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _pages_index_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../pages/index.css */ \"./src/pages/index.css\");\n/* harmony import */ var _marquee__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./marquee */ \"./src/components/marquee.js\");\n/* harmony import */ var _marquee__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_marquee__WEBPACK_IMPORTED_MODULE_1__);\n\n\n(function () {\n  const textElements = document.querySelectorAll('.about_text');\n  textElements.forEach(el => el.classList.remove('text_anim'));\n  const observer = new IntersectionObserver(entries => {\n    entries.forEach(entry => {\n      if (entry.isIntersecting) {\n        entry.target.classList.add('text_anim');\n      } else {\n        entry.target.classList.remove('text_anim');\n      }\n    });\n  });\n  textElements.forEach(el => observer.observe(el));\n}\n\n// function(){\n//   const header = document.querySelector('.header');\n//   header.classList.remove('header_main');\n//   const observer = new IntersectionObserver(entries =>{\n//     if(entries.isIntersecting){\n//       entries.classList.remove('header_main');\n//     }\n//     else{\n//       entries.classList.add('header_main')\n//     }\n//   });\n//   header(el => header.observe(el))\n// }\n)();\n\n//# sourceURL=webpack://seaforse/./src/components/index.js?\n}");
-
-/***/ }),
-
-/***/ "./src/components/marquee.js":
-/*!***********************************!*\
-  !*** ./src/components/marquee.js ***!
-  \***********************************/
-/***/ (() => {
-
-eval("{var wrapper = document.querySelector('.marquee-wrapper'),\n  marquee = document.querySelector('.marquee'),\n  wrapperWidth = wrapper.offsetWidth,\n  marqueeWidth = marquee.scrollWidth;\nfunction move() {\n  var currentTX = getComputedStyle(marquee).transform.split(',');\n  if (currentTX[4] === undefined) {\n    currentTX = -1;\n  } else {\n    currentTX = parseFloat(currentTX[4]) - 1;\n  }\n  if (-currentTX >= marqueeWidth) {\n    marquee.style.transform = 'translateX(' + wrapperWidth + 'px)';\n  } else {\n    marquee.style.transform = 'translateX(' + currentTX + 'px)';\n  }\n}\nvar interval = setInterval(move, 10);\n\n//# sourceURL=webpack://seaforse/./src/components/marquee.js?\n}");
+eval("{__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _pages_index_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../pages/index.css */ \"./src/pages/index.css\");\n\n\n// import \"./marquee\";\n\n(() => {\n  const textElements = document.querySelectorAll('.text_animation');\n  textElements.forEach(el => el.classList.remove('text_animation_start'));\n  const observer = new IntersectionObserver(entries => {\n    entries.forEach(entry => {\n      if (entry.isIntersecting) {\n        const delay = entry.target.dataset.delay || \"0s\";\n        entry.target.style.transitionDelay = delay;\n        entry.target.classList.add('text_animation_start');\n      } else {\n        entry.target.classList.remove('text_animation_start');\n        entry.target.style.transitionDelay = \"0s\";\n      }\n    });\n  });\n  textElements.forEach(el => observer.observe(el));\n})(), (() => {\n  const header = document.querySelector('.header');\n  const break_point = document.querySelector('.hero_page_but');\n  header.classList.remove('header_background');\n  const observer_revers = new IntersectionObserver(entries => {\n    entries.forEach(entry => {\n      if (entry.isIntersecting) {\n        header.classList.remove('header_background');\n      } else {\n        header.classList.add('header_background');\n      }\n    });\n  });\n  observer_revers.observe(break_point);\n})(), (() => {\n  document.addEventListener(\"DOMContentLoaded\", () => {\n    const sections = document.querySelectorAll(\".break_point\");\n    const navLinks = document.querySelectorAll(\".menu .link\");\n    const observer = new IntersectionObserver(entries => {\n      entries.forEach(entry => {\n        if (entry.isIntersecting) {\n          navLinks.forEach(link => link.classList.remove(\"active\"));\n          const activeLink = document.querySelector(`.menu .link[href=\"#${entry.target.getAttribute(\"name\")}\"]`);\n          if (activeLink) {\n            activeLink.classList.add(\"active\");\n          }\n        }\n      });\n    }, {\n      threshold: 0.99\n    });\n    sections.forEach(section => observer.observe(section));\n  });\n})();\n\n//# sourceURL=webpack://seaforse/./src/components/index.js?\n}");
 
 /***/ }),
 
@@ -36,7 +26,6 @@ eval("{var wrapper = document.querySelector('.marquee-wrapper'),\n  marquee = do
   \*****************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-"use strict";
 eval("{__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://seaforse/./src/pages/index.css?\n}");
 
 /***/ })
@@ -68,35 +57,6 @@ eval("{__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-ext
 /******/ 	}
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/compat get default export */
-/******/ 	(() => {
-/******/ 		// getDefaultExport function for compatibility with non-harmony modules
-/******/ 		__webpack_require__.n = (module) => {
-/******/ 			var getter = module && module.__esModule ?
-/******/ 				() => (module['default']) :
-/******/ 				() => (module);
-/******/ 			__webpack_require__.d(getter, { a: getter });
-/******/ 			return getter;
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/define property getters */
-/******/ 	(() => {
-/******/ 		// define getter functions for harmony exports
-/******/ 		__webpack_require__.d = (exports, definition) => {
-/******/ 			for(var key in definition) {
-/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
-/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
-/******/ 				}
-/******/ 			}
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
-/******/ 	(() => {
-/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/make namespace object */
 /******/ 	(() => {
 /******/ 		// define __esModule on exports
